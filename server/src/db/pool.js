bc4e27905d5263a665cbc97bfdcd756c3afd66e3
MaDGiiRL <sofiavidotto8@gmail.com>
2026-02-18 15:31:06 +0100

    push

--- server/src/db/pool.js
+++ server/src/db/pool.js
@@ -1,7 +1,17 @@
 // src/db/pool.js
 import pg from "pg";
+import dns from "node:dns";
 import { config } from "../config.js";
 
+function lookup(hostname, options, callback) {
+    return dns.lookup(hostname, { ...options, family: 4 }, callback);
+}
+
 export const pool = new pg.Pool({
-    connectionString: config.databaseUrl, // assicurati esista in config
+    connectionString: config.databaseUrl,
+    ssl: { rejectUnauthorized: false },
+    lookup,
+    max: 10,
+    idleTimeoutMillis: 30_000,
+    connectionTimeoutMillis: 10_000,
 });
